1f9431486e4855ea0c4940dcc1f388317a7f9673
허성준 <103251705+HeyHO2154@users.noreply.github.com>
2025-05-18 18:02:30 +0900

    Feat: 은하 유형 추가

--- Backend/object/galaxy.py
+++ Backend/object/galaxy.py
@@ -9,9 +9,11 @@ GALAXY_NAMES = load_txt("/DB/names/galaxy_names.txt")
 class Galaxy:
     def __init__(self):
         universe = read_json("/DB/InGame/Universes.json")[0]  # 현재 우주 상태
+        galaxy_types = read_json("/DB/models/galaxy_types.json")
         
         self.id = str(uuid.uuid4())
-        self.name = random.choice(GALAXY_NAMES)         
+        self.name = random.choice(GALAXY_NAMES)     
+        self.type = random.choice(galaxy_types)["id"] 
         self.create_time = universe['time']  # 현재 우주 시간
         self.update_time = universe['time']
 
@@ -26,4 +28,5 @@ def generate_galaxy():
     if random.random() < math.e/10: #자연상수 e = 2.718281828459045
         galaxy = Galaxy()
         create_json("/DB/InGame/Galaxies.json", galaxy)
-        print(f"🌌 {galaxy.name} 은하 탄생")
+        type_name = next(t["name"] for t in read_json("/DB/models/galaxy_types.json") if t["id"] == galaxy.type)    #디버깅용
+        print(f"🌌 {galaxy.name} {type_name} 은하 탄생")
