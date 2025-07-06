d1535c761819e50eb5617377e0c7ec1c741985c2
허성준 <103251705+HeyHO2154@users.noreply.github.com>
2025-07-06 14:16:43 +0900

    chore: MiniGuild repo와 병합

--- Frontend/app.js
+++
@@ -1,7 +0,0 @@
-import { loadStarsView } from "./map/stars.js";
-import { connectWebSocket } from "./websocket/websocket.js";
-
-window.addEventListener("DOMContentLoaded", () => {
-  connectWebSocket();
-  loadStarsView();
-});
