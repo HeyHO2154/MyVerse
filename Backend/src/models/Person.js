72d465ea57866525f16632f3a891816d32814698
허성준 <103251705+HeyHO2154@users.noreply.github.com>
2025-07-28 16:30:41 +0900

    feat: 개인 간소화, 가문 집중

--- Backend/src/models/Person.js
+++ Backend/src/models/Person.js
@@ -1,28 +1,12 @@
-const { randomUUID } = require('crypto');
-
 class Person {
-  constructor(gameState, dynasty, generation, sibling) {
-    this.id = randomUUID();
-    this.createdAt = gameState.year;
-    if(generation == 1){
-      this.createdAt -= 20; //최초 가문원은 성인부터 시작
-    }
-
-  
+  constructor(gameState, dynasty) {
     this.dynasty = dynasty;
+    this.createdAt = gameState.turn;
 
-    this.generation = generation;
-    this.gender = Math.random() < 0.5 ? 'male' : 'female';
+    this.gender = Math.random() < 0.5 ? '남' : '여';
     this.married = false;
-    this.child = 0; // 자녀에게 몇번째 자녀인지 부여
-    this.sibling = sibling; // 부모의 this.child를 받음, (이름: dynasty.name가문 generation세 sibling째)
-    this.name = dynasty.name+generation+"세"+sibling+(this.gender === 'male' ? "남" : "녀");
 
-    if(this.generation == 1){
-      console.log(`🧑‍ ${this.dynasty.name} 가문을 ${this.name}이(가) 창설했습니다.`);
-    }else{
-      console.log(`👶 ${this.dynasty.name} 가문에 ${this.name}이(가) 태어났습니다.`);
-    }
+    console.log(`👶 ${this.dynasty.name} 가문이 출산했습니다. (${this.gender})`);
   }
 
 }
--- Backend/src/models/Region.js
+++ Backend/src/models/Region.js
@@ -1,7 +1,7 @@
 class Region {
   constructor(gameState) {
     this.name = gameState.regionNames[Math.floor(Math.random() * gameState.regionNames.length)];
-    this.size = 2 + Math.floor(Math.random() * 5);
+    this.size = (2 + Math.floor(Math.random() * 5)) * 2;  // 4~12개(4:소형, 6:중소형, 8:중형, 10:중대형, 12:대형)
   
     // 자원을 무작위 순서로 배치 후 지역 크기(2~6)만큼 순차적으로 선택
     const allResources = ['석재', '철', '금', '석탄', '식량', '과일', '양모', '목재'];
@@ -9,14 +9,16 @@ class Region {
       .map(item => ({ item, random: Math.random() }))
       .sort((a, b) => a.random - b.random)
       .map(obj => obj.item)
-      .slice(0, this.size);
+      .slice(0, this.size/2);
 
     this.nation = null;
+    this.government = null;
+
     this.buildings = new Set(); // 각 지역마다 고유한 건물들
     this.dynasties = new Set(); // 각 지역마다 고유한 가문들
     
     gameState.regions.add(this);
-    console.log(`🌍 지역 ${this.name}이(가) 생성되었습니다. (크기: ${this.size}, 자원: ${this.resources.join(', ')})`);
+    console.log(`🌍 지역 ${this.name}이(가) 생성되었습니다. (크기: ${this.size})[${this.resources.join(', ')}]`);
     
   }
 
